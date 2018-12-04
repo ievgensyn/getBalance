ad78df70769406283f7e388bf271652167b80c30
ievgen <yevgens@web.de>
2018-12-04 18:42:34 +0200

    correction typo's

--- javascript/main.js
+++ javascript/main.js
@@ -4,15 +4,14 @@ function getBalance() {
 	var web3 = new Web3();
 	web3.setProvider(new web3.providers.HttpProvider( "http://188.190.240.195:8545"))
 	var address = document.getElementById("address").value;
-	console.log(address);
 	
 	try {
 		web3.eth.getBalance(address, function (error, wei) {
 			if (!error) {
 				var balance = web3.fromWei(wei, 'ether');
 				document.getElementById("output").innerHTML = balance + " ETH";
-				var gwei = web3.fromWei(wei, 'wei');
-				document.getElementById("output_wei").innerHTML = wei.toString(10) + " WEI";
+				var wei_balance = web3.fromWei(wei, 'wei');
+				document.getElementById("output_wei").innerHTML = wei_balance + " WEI";
 			}
 		})
 	} catch (err) {
