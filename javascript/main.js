abe6f1e171b553c962e7b6158d44b9e60e36f910
ievgen <yevgens@web.de>
2018-12-04 18:24:09 +0200

    add bignumber.js, update HttpProvider

--- javascript/main.js
+++ javascript/main.js
@@ -1,15 +1,19 @@
 function getBalance() {
 	'use strict';
-	var web3 = window.web3 ? new Web3(window.web3.currentProvider) : new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/E5WpJBGNnhJYUFLBattT"));
+	var Web3 = require('web3');
+	var web3 = new Web3();
+	web3.setProvider(new web3.providers.HttpProvider( "http://188.190.240.195:8545"))
 	var address = document.getElementById("address").value;
+	console.log(address);
 	
 	try {
 		web3.eth.getBalance(address, function (error, wei) {
 			if (!error) {
 				var balance = web3.fromWei(wei, 'ether');
 				document.getElementById("output").innerHTML = balance + " ETH";
+				var gwei = web3.fromWei(wei, 'wei');
+				document.getElementById("output_gwei").innerHTML = gwei.toString(10) + " WEI";
 			}
-//			console.log(balance.toString(10));
 		})
 	} catch (err) {
 		document.getElementById("output").innerHTML = err;
